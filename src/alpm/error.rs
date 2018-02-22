1f92da7031517bcfea043741d21f3dae2d8b191c
Jacob Humphrey <jacob.ryan.humphrey@gmail.com>
2018-02-22 01:26:51 -0600

    Moved errno_t to alpm/error.rs

--- src/alpm/error.rs
+++ src/alpm/error.rs
@@ -25,6 +25,90 @@ use super::*;
 //     }
 // }
 
+
+impl Default for errno_t {
+    fn default() -> Self {
+        errno_t::ALPM_ERR_OK
+    }
+}
+use std::fmt;
+impl fmt::Display for errno_t {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.alpm_strerror())
+    }
+}
+
+/// Error Codes
+#[derive(Debug, Copy, Clone)]
+pub enum errno_t {
+    ALPM_ERR_OK = 0,
+    ALPM_ERR_MEMORY,
+    ALPM_ERR_SYSTEM,
+    ALPM_ERR_BADPERMS,
+    ALPM_ERR_NOT_A_FILE,
+    ALPM_ERR_NOT_A_DIR,
+    ALPM_ERR_WRONG_ARGS,
+    ALPM_ERR_DISK_SPACE,
+    /* Interface */
+    ALPM_ERR_HANDLE_NULL,
+    ALPM_ERR_HANDLE_NOT_NULL,
+    ALPM_ERR_HANDLE_LOCK,
+    /* Databases */
+    ALPM_ERR_DB_OPEN,
+    ALPM_ERR_DB_CREATE,
+    ALPM_ERR_DB_NULL,
+    ALPM_ERR_DB_NOT_NULL,
+    ALPM_ERR_DB_NOT_FOUND,
+    ALPM_ERR_DB_INVALID,
+    ALPM_ERR_DB_INVALID_SIG,
+    ALPM_ERR_DB_VERSION,
+    ALPM_ERR_DB_WRITE,
+    ALPM_ERR_DB_REMOVE,
+    /* Servers */
+    ALPM_ERR_SERVER_BAD_URL,
+    ALPM_ERR_SERVER_NONE,
+    /* Transactions */
+    ALPM_ERR_TRANS_NOT_NULL,
+    ALPM_ERR_TRANS_NULL,
+    ALPM_ERR_TRANS_DUP_TARGET,
+    ALPM_ERR_TRANS_NOT_INITIALIZED,
+    ALPM_ERR_TRANS_NOT_PREPARED,
+    ALPM_ERR_TRANS_ABORT,
+    ALPM_ERR_TRANS_TYPE,
+    ALPM_ERR_TRANS_NOT_LOCKED,
+    ALPM_ERR_TRANS_HOOK_FAILED,
+    /* Packages */
+    ALPM_ERR_PKG_NOT_FOUND,
+    ALPM_ERR_PKG_IGNORED,
+    ALPM_ERR_PKG_INVALID,
+    ALPM_ERR_PKG_INVALID_CHECKSUM,
+    ALPM_ERR_PKG_INVALID_SIG,
+    ALPM_ERR_PKG_MISSING_SIG,
+    ALPM_ERR_PKG_OPEN,
+    ALPM_ERR_PKG_CANT_REMOVE,
+    ALPM_ERR_PKG_INVALID_NAME,
+    ALPM_ERR_PKG_INVALID_ARCH,
+    ALPM_ERR_PKG_REPO_NOT_FOUND,
+    /* Signatures */
+    ALPM_ERR_SIG_MISSING,
+    ALPM_ERR_SIG_INVALID,
+    /* Deltas */
+    ALPM_ERR_DLT_INVALID,
+    ALPM_ERR_DLT_PATCHFAILED,
+    /* Dependencies */
+    ALPM_ERR_UNSATISFIED_DEPS,
+    ALPM_ERR_CONFLICTING_DEPS,
+    ALPM_ERR_FILE_CONFLICTS,
+    /* Misc */
+    ALPM_ERR_RETRIEVE,
+    ALPM_ERR_INVALID_REGEX,
+    /* External library errors */
+    ALPM_ERR_LIBARCHIVE,
+    ALPM_ERR_LIBCURL,
+    ALPM_ERR_EXTERNAL_DOWNLOAD,
+    ALPM_ERR_GPGME,
+}
+
 impl errno_t {
     pub fn alpm_strerror(&self) -> String {
         use self::errno_t::*;
--- src/alpm/mod.rs
+++ src/alpm/mod.rs
@@ -46,6 +46,7 @@ pub use self::db::alpm_db_t;
 pub use self::deps::alpm_dep_from_string;
 pub use self::be_sync::alpm_db_update;
 pub use self::deps::alpm_find_satisfier;
+pub use self::error::errno_t;
 
 const SYSHOOKDIR: &str = "/usr/local/share/libalpm/hooks/";
 
@@ -101,89 +102,6 @@ pub type Result<T> = std::result::Result<T, errno_t>;
 // typedef struct __alpm_db_t alpm_db_t;
 // typedef struct __pkg_t pkg_t;
 // typedef struct __alpm_trans_t alpm_trans_t;
-
-impl Default for errno_t {
-    fn default() -> Self {
-        errno_t::ALPM_ERR_OK
-    }
-}
-use std::fmt;
-impl fmt::Display for errno_t {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.alpm_strerror())
-    }
-}
-
-/// Error Codes
-#[derive(Debug, Copy, Clone)]
-pub enum errno_t {
-    ALPM_ERR_OK = 0,
-    ALPM_ERR_MEMORY,
-    ALPM_ERR_SYSTEM,
-    ALPM_ERR_BADPERMS,
-    ALPM_ERR_NOT_A_FILE,
-    ALPM_ERR_NOT_A_DIR,
-    ALPM_ERR_WRONG_ARGS,
-    ALPM_ERR_DISK_SPACE,
-    /* Interface */
-    ALPM_ERR_HANDLE_NULL,
-    ALPM_ERR_HANDLE_NOT_NULL,
-    ALPM_ERR_HANDLE_LOCK,
-    /* Databases */
-    ALPM_ERR_DB_OPEN,
-    ALPM_ERR_DB_CREATE,
-    ALPM_ERR_DB_NULL,
-    ALPM_ERR_DB_NOT_NULL,
-    ALPM_ERR_DB_NOT_FOUND,
-    ALPM_ERR_DB_INVALID,
-    ALPM_ERR_DB_INVALID_SIG,
-    ALPM_ERR_DB_VERSION,
-    ALPM_ERR_DB_WRITE,
-    ALPM_ERR_DB_REMOVE,
-    /* Servers */
-    ALPM_ERR_SERVER_BAD_URL,
-    ALPM_ERR_SERVER_NONE,
-    /* Transactions */
-    ALPM_ERR_TRANS_NOT_NULL,
-    ALPM_ERR_TRANS_NULL,
-    ALPM_ERR_TRANS_DUP_TARGET,
-    ALPM_ERR_TRANS_NOT_INITIALIZED,
-    ALPM_ERR_TRANS_NOT_PREPARED,
-    ALPM_ERR_TRANS_ABORT,
-    ALPM_ERR_TRANS_TYPE,
-    ALPM_ERR_TRANS_NOT_LOCKED,
-    ALPM_ERR_TRANS_HOOK_FAILED,
-    /* Packages */
-    ALPM_ERR_PKG_NOT_FOUND,
-    ALPM_ERR_PKG_IGNORED,
-    ALPM_ERR_PKG_INVALID,
-    ALPM_ERR_PKG_INVALID_CHECKSUM,
-    ALPM_ERR_PKG_INVALID_SIG,
-    ALPM_ERR_PKG_MISSING_SIG,
-    ALPM_ERR_PKG_OPEN,
-    ALPM_ERR_PKG_CANT_REMOVE,
-    ALPM_ERR_PKG_INVALID_NAME,
-    ALPM_ERR_PKG_INVALID_ARCH,
-    ALPM_ERR_PKG_REPO_NOT_FOUND,
-    /* Signatures */
-    ALPM_ERR_SIG_MISSING,
-    ALPM_ERR_SIG_INVALID,
-    /* Deltas */
-    ALPM_ERR_DLT_INVALID,
-    ALPM_ERR_DLT_PATCHFAILED,
-    /* Dependencies */
-    ALPM_ERR_UNSATISFIED_DEPS,
-    ALPM_ERR_CONFLICTING_DEPS,
-    ALPM_ERR_FILE_CONFLICTS,
-    /* Misc */
-    ALPM_ERR_RETRIEVE,
-    ALPM_ERR_INVALID_REGEX,
-    /* External library errors */
-    ALPM_ERR_LIBARCHIVE,
-    ALPM_ERR_LIBCURL,
-    ALPM_ERR_EXTERNAL_DOWNLOAD,
-    ALPM_ERR_GPGME,
-}
 
 // /** Returns the current error code from the handle. */
 // errno_t alpm_errno(alpm_handle_t *handle);
